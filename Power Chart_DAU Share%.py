7312be9720db37cc2e28b7e7f15dcb30d0d30ea1
AnikaNC <87225806+AnikaNChow@users.noreply.github.com>
2023-04-10 21:22:34 +0600

    first

--- Power Chart_DAU Share%.py
+++ Power Chart_DAU Share%.py
@@ -1,14 +1,23 @@
 
 
-DAU Share>>>>>>>>>>>Plot the DataFrame using Pandas:
+DAU Share>>>>>>>>>>> Plot the DataFrame using Pandas:
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = {'Week': [1,2,3,4],
-        'DAU_Share %': [2.5,2.5,2.5,3.4]
+data = {'Week': [1, 2, 3, 4],
+        'DAU_Share %': [2.1, 2.4, 3.4, 3.4]
        }
   
-df = pd.DataFrame(data,columns=['Week','DAU_Share %'])
-df.plot(x ='Week', y='DAU_Share %', kind = 'bar')
+df = pd.DataFrame(data, columns=['Week', 'DAU_Share %'])
+ax = df.plot(x='Week', y='DAU_Share %', kind='bar', align='edge', width=0.7)
+
+ax.set_title('Daily Active Users:\nE-Commerce Platform', fontsize=12, ha='center', va='bottom')
+ax.set_xlabel('Week', fontsize=10)
+ax.set_ylabel('DAU Share %', fontsize=10)
+
+note_text = 'Note: The chart illustrates DAU share percentage for an E-Commerce platform over four weeks.'
+ax.text(0.5, -0.20, note_text, ha='center', fontsize=10, transform=ax.transAxes,
+        bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
+
 plt.show()
--- Steps of Creating Pd Dataframe.py
+++ Steps of Creating Pd Dataframe.py
@@ -2,31 +2,60 @@
 
 import pandas as pd
    
-data = {'Unemployment_Rate': [6.1,5.8,5.7,5.7,5.8,5.6,5.5,5.3,5.2,5.2],
-        'Stock_Index_Price': [1500,1520,1525,1523,1515,1540,1545,1560,1555,1565]
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = {'Week': [1, 2, 3, 4],
+        'DAU_Share %': [2.1, 2.4, 3.4, 3.4]
        }
-df = pd.DataFrame(data,columns=['Unemployment_Rate','Stock_Index_Price'])
+  
+df = pd.DataFrame(data, columns=['Week', 'DAU_Share %'])
 print (df)
 
 
 -------Step 2: Plot the DataFrame using Pandas
 Finally, you can plot the DataFrame by adding the following syntax:
 
-df.plot(x ='Unemployment_Rate', y='Stock_Index_Price', kind = 'scatter')	
+ax = df.plot(x='Week', y='DAU_Share %', kind='bar', align='edge', width=0.7)
 
+
+--------3 Adding title and note/insights to your power charts (with x-axis and y-axis labels )
+
+
+ax.set_title('Daily Active Users:\nE-Commerce Platform', fontsize=12, ha='center', va='bottom')
+ax.set_xlabel('Week', fontsize=10)
+ax.set_ylabel('DAU Share %', fontsize=10)
+
+note_text = 'Note: The chart illustrates DAU share percentage for an E-Commerce platform over four weeks.'
+ax.text(0.5, -0.20, note_text, ha='center', fontsize=10, transform=ax.transAxes,
+        bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 
 
 ----Final Step: Putting everything together:
 
 import pandas as pd
 import matplotlib.pyplot as plt
-   
-data = {'Unemployment_Rate': [6.1,5.8,5.7,5.7,5.8,5.6,5.5,5.3,5.2,5.2],
-        'Stock_Index_Price': [1500,1520,1525,1523,1515,1540,1545,1560,1555,1565]
-       }
-df = pd.DataFrame(data,columns=['Unemployment_Rate','Stock_Index_Price'])
-df.plot(x ='Unemployment_Rate', y='Stock_Index_Price', kind = 'scatter')
-plt.show()
 
+
+data = {'Week': [1, 2, 3, 4],
+        'DAU_Share %': [2.1, 2.4, 3.4, 3.4]
+       }
+  
+
+df = pd.DataFrame(data, columns=['Week', 'DAU_Share %'])
+ax = df.plot(x='Week', y='DAU_Share %', kind='bar', align='edge', width=0.7)
+
+
+ax.set_title('Daily Active Users:\nE-Commerce Platform', fontsize=12, ha='center', va='bottom')
+ax.set_xlabel('Week', fontsize=10)
+ax.set_ylabel('DAU Share %', fontsize=10)
+
+
+note_text = 'Note: The chart illustrates DAU share percentage for an E-Commerce platform over four weeks.'
+ax.text(0.5, -0.20, note_text, ha='center', fontsize=10, transform=ax.transAxes,
+        bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
+
+
+plt.show()
 
 
